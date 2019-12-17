1cd51c6cee11007c2e72ad770afce14e09659727
priyesh08 <priyesh.nagar1995@gmail.com>
2019-12-17 23:43:09 +0530

    final commit

--- src/server.js
+++ src/server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/UI'));
+app.use(express.static(__dirname + '/dist/ui'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/UI/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/ui/index.html'));
 });
 
 // default Heroku PORT
